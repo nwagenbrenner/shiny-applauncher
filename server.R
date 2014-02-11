dc86cea27c396703c2e21a513b1a90946ee083af
natalie <nwagenbrenner@gmail.com>
2014-02-10 22:34:57 -0700

    add server.R and ui.R

---
+++ server.R
@@ -0,0 +1,14 @@
+library(shiny)
+
+#set some initial data
+
+# Define server logic
+shinyServer(function(input, output) {
+  
+  # Compute the forumla text in a reactive expression
+
+  # Return the formula text for printing as a caption
+
+
+})
+
