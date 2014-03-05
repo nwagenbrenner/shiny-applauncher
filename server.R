f0a677ec5aa5fd17dbc4ccccfcd73e89953d329a
nwagenbrenner <nwagenbrenner@gmail.com>
2014-03-05 11:45:50 -0700

    remove escape character

--- server.R
+++ server.R
@@ -12,20 +12,17 @@ shinyServer(function(input, output, session) {
       system(paste0("cp windninja/ui.R ../userWork/", uuid))
       system(paste0("cp -r windninja/www/ ../userWork/", uuid))
       system(paste0("cp windninja/ascii2vectorsSP.R ../userWork/", uuid))
-      projectName <- gsub(" ", "/ ", input$project)
-      system2("./mailMessage.bash", c(input$email, uuid, "WindNinja", "webNinja", projectName))
+      system2("./mailMessage.bash", c(input$email, uuid, "WindNinja", "webNinja", input$project))
       h4("WindNinja project created! An email has been sent with the link to your project page.")
   }
   makeFVS<-function(){
       system(paste0("cp -r fvs/* ../userWork/", uuid))
-      projectName <- gsub(" ", "/ ", input$project)
-      system2("./mailMessage.bash", c(input$email, uuid, "FVS", "webFVS", projectName))
+      system2("./mailMessage.bash", c(input$email, uuid, "FVS", "webFVS", input$project))
       h4("FVS project created! An email has been sent with the link to your project page.")
   }
   makeHiresWind<-function(){
       system(paste0("cp -r hiresWind/* ../userWork/", uuid))
-      projectName <- gsub(" ", "/ ", input$project)
-      system2("./mailMessage.bash", c(input$email, uuid, "WindData", "webWindData", projectName))
+      system2("./mailMessage.bash", c(input$email, uuid, "WindData", "webWindData", input$project))
       h4("Wind Data project created! An email has been sent with the link to your project page.")
   }
 
